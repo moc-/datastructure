7d34f6c7d2079716b557b28c4191ad9bfb33f2bd
moguzh <gzh_moc@163.com>
2012-05-03 14:15:56 +0800

    modified to a clear format

--- seq_b_tree/btree.h
+++ seq_b_tree/btree.h
@@ -16,51 +16,51 @@
 
 
 
-typedef t_elem_type sq_b_tree[MAX_TREE_SIZE];
+typedef Elem_type Sq_b_tree[MAX_TREE_SIZE];
 
 struct Pos
 {
     int level;
     int order;
 };
-typedef  struct Pos position;
+typedef  struct Pos Position;
 //创建及销毁
-Status  init_b_tree(sq_b_tree T);   // 构造空btree,T是定长数组，
+Status  init_b_tree(Sq_b_tree T);   // 构造空btree,T是定长数组，
                                     // 无需 &
-void    destory_b_tree();           // sq_b_tree 是定长类型，
+void    destory_b_tree();           // Sq_b_tree 是定长类型，
                                     // 无需销毁
-Status  create_b_tree(sq_b_tree T); // 构造顺序存储的btree
-Status  b_tree_empty(sq_b_tree T);  // btree 是否空的
+Status  create_b_tree(Sq_b_tree T); // 构造顺序存储的btree
+Status  b_tree_empty(Sq_b_tree T);  // btree 是否空的
 
 //辅助函数
-int b_tree_depth(sq_b_tree T);      
-Status  root(sq_b_tree T, t_elem_type *value);
-void    move(sq_b_tree q, int q_order, sq_b_tree T, int T_order );
+int b_tree_depth(Sq_b_tree T);      
+Status  root(Sq_b_tree T, Elem_type *value);
+void    move(Sq_b_tree q, int q_order, Sq_b_tree T, int T_order );
 
-void    pre_traverse(sq_b_tree T, int index);
-void    in_traverse(sq_b_tree T, int index);
-void    post_traverse(sq_b_tree T, int index);
-void    print(sq_b_tree T);
+void    pre_traverse(Sq_b_tree T, int index);
+void    in_traverse(Sq_b_tree T, int index);
+void    post_traverse(Sq_b_tree T, int index);
+void    print(Sq_b_tree T);
 
 //update
-Status  assign(sq_b_tree T, position pos,  t_elem_type value);
+Status  assign(Sq_b_tree T, Position pos,  Elem_type value);
 // query
-t_elem_type value(sq_b_tree T, position pos);
-t_elem_type parent(sq_b_tree T, t_elem_type node);
-t_elem_type lchild(sq_b_tree T, t_elem_type node);
-t_elem_type rchild(sq_b_tree T, t_elem_type node);
-t_elem_type lsibling(sq_b_tree T, t_elem_type node);
-t_elem_type rsibling(sq_b_tree T, t_elem_type node);
+Elem_type value(Sq_b_tree T, Position pos);
+Elem_type parent(Sq_b_tree T, Elem_type node);
+Elem_type lchild(Sq_b_tree T, Elem_type node);
+Elem_type rchild(Sq_b_tree T, Elem_type node);
+Elem_type lsibling(Sq_b_tree T, Elem_type node);
+Elem_type rsibling(Sq_b_tree T, Elem_type node);
 
-Status  pre_order_traverse(sq_b_tree T, Status (*visit)(t_elem_type));
-Status  in_order_traverse(sq_b_tree T, Status (*visit)(t_elem_type));
-Status  post_order_traverse(sq_b_tree T, Status (*visit)(t_elem_type));
+Status  pre_order_traverse(Sq_b_tree T, Status (*visit)(Elem_type));
+Status  in_order_traverse(Sq_b_tree T, Status (*visit)(Elem_type));
+Status  post_order_traverse(Sq_b_tree T, Status (*visit)(Elem_type));
 
 //insert
-Status  insert_child(sq_b_tree T, t_elem_type node, Status lr, 
-                        sq_b_tree c );
+Status  insert_child(Sq_b_tree T, Elem_type node, Status lr, 
+                        Sq_b_tree c );
 //delete
-Status  delete_child(sq_b_tree T, position pos, Status lr);
+Status  delete_child(Sq_b_tree T, Position pos, Status lr);
 
 
 
